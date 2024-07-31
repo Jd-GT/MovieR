c2b2b95b7004ebd59fb52978e42644109437e2b8
J_SC <juansaca360@gmail.com>
2024-07-31 08:32:47 -0500

    creacion de la pagina web de  moviereview

--- movie/views.py
+++ movie/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
+from .models import Movie
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html',{'name':'El Gran david'})
+
+    searchTerm =request.GET.get('searchMovie')
+    if searchTerm:
+        movies = Movie.objects.filter(title__icontains=searchTerm)
+    else:
+        movies= Movie.objects.all()
+    return render(request, 'home.html', {'searchTerm':searchTerm, 'movies':movies})
+    # return render(request, 'home.html',{'name':'El Gran david'})
 
 def about(request):
     return render(request, 'about.html')
-
+    
 def abou(request):
     return render(request, 'abou.html')
